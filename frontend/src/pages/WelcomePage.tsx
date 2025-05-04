5cbccb45f14c5e4bc89f99505f46032ba670d4b3
samuelberven <samberven@gmail.com>
2025-05-03 22:35:35 -0700

    Change Welcome Page name to LiftLog

--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -13,7 +13,7 @@ const WelcomePage: React.FC = () => {
   return (
     <div className="min-h-screen bg-indigo-600 text-white flex flex-col">
       <div className="container mx-auto px-4 py-8 flex-grow flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold mb-8">Welcome to Lift Logger</h1>
+        <h1 className="text-6xl font-bold mb-8">Welcome to LiftLog</h1>
         <p className="mb-4 text-xl">Track your gains and push your limits.</p>
         <p className="mb-12 text-sm italic">
           (Note: User login features are still under development. Go ahead and press the button to proceed!)
